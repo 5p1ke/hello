7b14b1640cebe685d3a629769d3aa9c4d3ad8ff2
5p1ke <amarchenko@speroteck.com>
2017-05-30 18:29:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!two')
+  res.send('Hello World!three')
 })
 
 app.listen(3000, function () {
